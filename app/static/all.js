f31d86939c5ed63045db3f331fba85b24daf2de8
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2015-10-29 15:27:58 +0100

    Wait until images are loaded before fixing div height

--- app/static/all.js
+++ app/static/all.js
@@ -1,4 +1,4 @@
-$(document).ready(function() { 
+$(window).load(function() { 
     h = Math.max($(".two-thirds-column").height(), $(".one-third-column").height());
     $(".two-thirds-column").height(h);
     $(".one-third-column").height(h);
